f6862fd881375d55b48d712067fee7cf87310d3c
Diana Arcanjo <dianaarcanjo@MacBook-Pro-de-Diana.local>
2024-09-30 08:46:04 -0300

    first commit

---
+++ EspecialistaJava/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Cliente cliente = new Cliente();
+        cliente.idade = Integer.valueOf(10);
+
+        System.out.println(cliente.idade);
+
+        }
+    }
