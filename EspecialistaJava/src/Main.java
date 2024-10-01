53faa25b90d428ab4c98c459490b0a5261a5ceb7
dianaarcanjo <dianaarcanjo.0@gmail.com>
2024-10-01 08:35:51 -0300

    Commit array

--- EspecialistaJava/src/Main.java
+++ EspecialistaJava/src/Main.java
@@ -1,12 +1,27 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         Cliente cliente = new Cliente();
-        cliente.idade = Integer.valueOf(10);
+        cliente.idade = 10;
+        cliente.notas = new int[]{2, 3, 4, 5};
 
-        System.out.println(cliente.idade);
+        //Printando o valor por index
+        System.out.println(cliente.notas[2]);
 
+        for (int element : cliente.notas) {
+            System.out.println(element);
         }
+        System.out.println(cliente.notas[2]);
+
+        //Alterando os valores do index
+        cliente.notas[3]= 10;
+        System.out.println(cliente.notas[3]);
+
+
+
     }
+}
